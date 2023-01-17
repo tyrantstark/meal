f4c5438a815950abe7c44acbf8e3cab7082bf0e5
Abhishek kumar <ak749258@gmail.com>
2023-01-18 03:19:01 +0530

    poppppl

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === "production") {
   // static folder
   app.use(express.static("../dist"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../dist/index.html'));
+    res.sendFile(__dirname + '../dist/index.html');
   });
 }
 app.use(express.static(path.join(__dirname, '../public/index.html')))
